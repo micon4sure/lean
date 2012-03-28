3ed32e6a7450ae78f4a754da93f32901f5c31ece
muffomat <muff@om.at>
2012-03-28 17:16:12 +0200

    Radio labels

--- lean/lib/form/element/radio.php
+++ lean/lib/form/element/radio.php
@@ -30,7 +30,7 @@ class Radio extends \lean\form\Element {
         $title = $this->options[$key];
         if(mb_strlen(trim($title)) > 0)
             $label = sprintf('<label for="%s_%s">%s</label>', $this->getId(), $key, $title);
-        printf('<input %1$s type="radio" name="%2$s_%3$s" id="%2$s" value="%3$s" %4$s />%5$s', $this->getAttributeString(), $this->getId(), $key, $key == $this->getValue() ? ' checked="checked"' : '', $label);
+        printf('<input %1$s type="radio" name="%2$s" id="%2$s_%3$s" value="%3$s" %4$s />%5$s', $this->getAttributeString(), $this->getId(), $key, $key == $this->getValue() ? ' checked="checked"' : '', $label);
         return $this;
     }
 }
