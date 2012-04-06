47ca6c0b1fc25708df6b4415c8738da688542b09
klawd-prime <klawd+git@kamundo.de>
2012-04-06 21:42:30 +0200

    Moved Util_Datetime to util namespace

--- lean/lib/util/datetime.php
+++ lean/lib/util/datetime.php
@@ -1,23 +1,44 @@
 <?php
-namespace lean;
+namespace lean\util;
 
-class Util_DateTime extends \DateTime {
+/**
+ * Convinience layer for the built-in datetime class
+ */
+class DateTime extends \DateTime {
+    /**
+     * @return string
+     */
     public function getYear() {
         return $this->format('Y');
     }
+    /**
+     * @return string
+     */
     public function getMonth() {
         return $this->format('m');
     }
+    /**
+     * @return string
+     */
     public function getDay() {
         return $this->format('d');
     }
 
+    /**
+     * @return string
+     */
     public function getHour() {
         return $this->format('H');
     }
+    /**
+     * @return string
+     */
     public function getMinute() {
         return $this->format('i');
     }
+    /**
+     * @return string
+     */
     public function getSecond() {
         return $this->format('s');
     }
