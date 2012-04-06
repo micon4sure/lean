15abe65b85597152ae9ed026d09b6702f7db1061
klawd-prime <klawd+git@kamundo.de>
2012-04-06 22:32:55 +0200

    Code style

--- lean/init.php
+++ lean/init.php
@@ -48,20 +48,21 @@ class Autoload {
      */
     public function load($name) {
         foreach ($this->libraries as $namespace => $directory) {
-            if (substr($name, 0, strlen($namespace)) == $namespace) {
-                $subName = substr($name, strlen($namespace) + 1); // +1 for leading backspace
-                $tree = explode('\\', $subName);
-                $item = array_pop($tree);
-                foreach ($tree as $subdir) {
-                    $directory .= '/' . strtolower($subdir);
-                }
+            if (substr($name, 0, strlen($namespace)) != $namespace) {
+                continue;
+            }
+            $subName = substr($name, strlen($namespace) + 1); // +1 for leading backspace
+            $tree = explode('\\', $subName);
+            $item = array_pop($tree);
+            foreach ($tree as $subdir) {
+                $directory .= '/' . strtolower($subdir);
+            }
 
-                $itemParts = explode('_', $item);
-                $path = sprintf('%s/%s.php', $directory, strtolower($itemParts[0]));
+            $itemParts = explode('_', $item);
+            $path = sprintf('%s/%s.php', $directory, strtolower($itemParts[0]));
 
-                if (file_exists($path)) {
-                    require_once($path);
-                }
+            if (file_exists($path)) {
+                require_once($path);
             }
         }
     }
